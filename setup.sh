65673bd1aeb10c78780a462e528ce9535d00008b
c-Gah <ryon.c.ballard@gmail.com>
2023-05-25 10:52:16 -0400

    changing storage of llama from google drive to google cloud storage

--- setup.sh
+++ setup.sh
@@ -3,20 +3,20 @@
 # Clone the GitHub repo "https://github.com/c-Gah/MiniGPT-4" into /usr/local/minigpt4
 git clone https://github.com/c-Gah/MiniGPT-4 /usr/local/minigpt4
 
-# Create a group named "minigpt"
-sudo groupadd minigpt
+# Create a group named "minigpt4_g"
+sudo groupadd minigpt4_g
 
-# Create a user named "minigpt" with no login shell and belonging to the "minigpt" group
-sudo useradd -r -s /bin/false -g minigpt minigpt
+# Create a user named "minigpt4_u" with no login shell and belonging to the "minigpt4_g" group
+sudo useradd -r -s /bin/false -g minigpt4_g minigpt4_u
 
-# Set ownership of /usr/local/minigpt/ to the "minigpt" user and group
-sudo chown -R minigpt:minigpt /usr/local/minigpt/
+# Set ownership of /usr/local/minigpt4/ to the "minigpt4_u" user and group
+sudo chown -R minigpt4_u:minigpt4_g /usr/local/minigpt4/
 
-# Set permissions on /home/minigpt to 755 recursively
-sudo chmod 755 -R /home/minigpt
+# Set permissions on /home/minigpt4_u to 755 recursively
+sudo chmod 755 -R /home/minigpt4_u
 
-# Set permissions on /home/minigpt to 775 recursively
-sudo chmod 775 -R /home/minigpt
+# Set permissions on /home/minigpt4_u to 775 recursively
+sudo chmod 775 -R /home/minigpt4_u
 
 # Install nano text editor
 sudo apt-get update
@@ -25,17 +25,13 @@ sudo apt-get install -y nano
 # Install uvicorn
 sudo apt-get install -y uvicorn
 
-# Install gdown using pip3
-sudo pip3 install gdown
-
-# Download the llama-7b-minigpt.tar file from Google Drive using gdown
-file_id="1vbpJ9cRxZPTQMMs9N6UAE2ygBP85ct3U"
-file_name="llama-7b-minigpt.tar"
-output_file="/usr/local/minigpt4/llama/$file_name"
-gdown --id "$file_id" --output "$output_file"
-
 # Create the /usr/local/minigpt4/llama directory
 sudo mkdir -p /usr/local/minigpt4/llama
+
+# Download the llama-7b-minigpt.tar file from Google Cloud Storage using wget
+file_name="llama-7b-minigpt.tar"
+output_file="/usr/local/minigpt4/llama/$file_name"
+sudo wget https://storage.googleapis.com/poc_llama/llama-7b-minigpt.tar -O "$output_file"
 
 # Extract the contents of the llama-7b-minigpt.tar file to /usr/local/minigpt4/llama
 sudo tar -xf "$output_file" -C /usr/local/minigpt4/llama
@@ -48,9 +44,9 @@ After=network.target
 
 [Service]
 ExecStart=/opt/conda/envs/minigpt4/bin/uvicorn main:app --host 0.0.0.0 --port 8000
-WorkingDirectory=/home/minigpt/
-User=minigpt
-Group=minigpt
+WorkingDirectory=/home/minigpt4_u/
+User=minigpt4_u
+Group=minigpt4_g
 Restart=on-failure
 RestartSec=360s
 
@@ -60,8 +56,8 @@ EOF
 
 # Enable the minigpt service to start on boot
 # sudo systemctl enable minigpt.service
-sudo service minigpt enable
+sudo systemctl enable minigpt
 
 # Start the minigpt service
 # sudo systemctl start minigpt.service
-sudo service minigpt start
+sudo systemctl start minigpt
