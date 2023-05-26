8512eb90921b5a5b1eff2b462cd943ad70d18255
c-Gah <ryon.c.ballard@gmail.com>
2023-05-26 00:06:06 -0400

    script fix

--- setup.sh
+++ setup.sh
@@ -13,25 +13,17 @@ sudo useradd -r -s /bin/false -g minigpt4_g minigpt4_u
 sudo chown -R minigpt4_u:minigpt4_g /usr/local/minigpt4/
 
 # Set permissions on /home/minigpt4_u to 755 recursively
-sudo chmod 755 -R /home/minigpt4_u
+# sudo chmod 755 -R /home/minigpt4_u
 
 # Set permissions on /home/minigpt4_u to 775 recursively
-sudo chmod 775 -R /home/minigpt4_u
+sudo chmod 775 -R /usr/local/minigpt4
 
 # Install nano text editor
 sudo apt-get update
 sudo apt-get install -y nano
+sudo apt-get install -y build-essential
+sudo apt-get install -y libgl1-mesa-glx
 
-file_name="Anaconda3-2023.03-1-Linux-x86_64.sh"
-output_file="/usr/local/$file_name"
-sudo wget https://repo.anaconda.com/archive/Anaconda3-2023.03-1-Linux-x86_64.sh -O "$output_file"
-
-bash "/usr/local/$file_name" -b -p /usr/local/anaconda3
-
-sudo rm "/usr/local/$file_name"
-
-sudo /usr/local/anaconda3/bin/conda env create -f /usr/local/minigpt4/environment.yml
-sudo /usr/local/anaconda3/bin/conda activate minigpt4
 
 # Create the /usr/local/minigpt4/llama directory
 sudo mkdir -p /usr/local/minigpt4/llama/7b
@@ -49,6 +41,24 @@ sudo tar -xf "$output_file" -C /usr/local/minigpt4/llama/7b
 file_name="prerained_minigpt4_7b.pth"
 output_file="/usr/local/minigpt4/llama/checkpoint/$file_name"
 sudo wget https://storage.googleapis.com/poc_llama/prerained_minigpt4_7b.pth -O "$output_file"
+
+# file_name="Anaconda3-2023.03-1-Linux-x86_64.sh"
+# output_file="/usr/local/$file_name"
+# sudo wget https://repo.anaconda.com/archive/Anaconda3-2023.03-1-Linux-x86_64.sh -O "$output_file"
+# bash "/usr/local/$file_name" -b -p /usr/local/anaconda3
+# sudo rm "/usr/local/$file_name"
+
+file_name="miniconda.sh"
+output_file="/usr/local/$file_name"
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O "$output_file"
+bash "$output_file" -b -p /usr/local/miniconda
+sudo rm "/usr/local/$file_name"
+
+export PATH="/usr/local/miniconda/bin:/usr/local/miniconda/condabin:$PATH"
+
+conda env create -f /usr/local/minigpt4/environment.yml
+conda init bash
+conda activate minigpt4
 
 # Create the minigpt.service file with the updated service contents
 cat <<EOF | sudo tee /etc/systemd/system/minigpt.service
